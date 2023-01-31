9045ac13e397bc2091f4f7ded63b334b48f2b73e
Your Name <you@example.com>
2023-01-31 01:24:00 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    
+    'author': 'Roger',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+    ],
+    'demo': [
+        '',
+    ],
+    #Add license to remove License Warning
+    'license': 'OPL-1'
+}
